e126dc6c89c47d70b39d39e94a785216a46cab46
Elias Pfaeffli <eliaspfaeffli@hotmail.com>
2024-06-20 02:09:24 -0300

    Entrega 2 Javascript

--- js/main.js
+++ js/main.js
@@ -1,62 +1,123 @@
-// Simulador cajero 
+// Simulador cajero
 
-// let usuario = prompt("Ingrese su usuario:");
-// let contrasenia = prompt("Ingrese su PIN:");
+const Cajero = {
+    usuario: "",
+    contrasenia: "",
+    balancePesos: 287300,
+    balanceUsd: 238,
+    extraccionPesos: 0,
+    extraccionUsd: 0,
+    transferencia: 0,
+    historialTransacciones: [],
 
-// alert("Bienvenido " + usuario);
+    iniciarSesion: function() {
+        this.usuario = prompt("Ingrese su usuario:");
+        this.contrasenia = prompt("Ingrese su PIN:");
+        alert("Bienvenido " + this.usuario);
+    },
 
-let balancePesos = 287300; 
-let balanceUsd = 238;
-let extraccionPesos = 0;
-let extraccionUsd = 0; 
-let trasferencia = 0;
+    gestionarOperacion: function(operacion) {
+        switch (operacion) {
+            case "a":
+                this.gestionarTransferenciaDeposito();
+                break;
+            case "b":
+                this.gestionarExtraccion();
+                break;
+            case "c":
+                this.consultarSaldo();
+                break;
+            case "d":
+                this.verHistorialTransacciones();
+                break;
+            default:
+                alert("Opción no válida");
+        }
+    },
 
-function gestionarOperacion(operacion) {
+    gestionarTransferenciaDeposito: function() {
+        let operacionA = prompt("A) Transferencia \nB) Depósito \nV) Volver al menú principal").toLowerCase();
+        switch (operacionA) {
+            case "a":
+                prompt("Ingrese Alias o CBU");
+                this.transferencia = parseFloat(prompt("Ingrese monto a transferir:"));
+                this.balancePesos -= this.transferencia;
+                this.historialTransacciones.push(`Transferencia de $${this.transferencia}`);
+                alert("¡Transferencia enviada!");
+                break;
+            case "b":
+                prompt("Ingrese número de cuenta a depositar:");
+                let deposito = parseFloat(prompt("Ingrese monto a depositar:"));
+                this.historialTransacciones.push(`Depósito de $${deposito}`);
+                alert("¡Depositado con éxito!");
+                break;
+            case "v":
+                break;
+            default:
+                alert("Opción no válida");
+        }
+    },
 
-    if (operacion === "a") {
-        let operacionA = prompt("A) Transferencia \nB) Deposito \nV) Volver al menú principal");
-        operacionA = operacionA.toLowerCase();
-        if (operacionA === "a") {
-            prompt("Ingrese Alias o CBU");
-            trasferencia =parseFloat(prompt("Ingrese monto a trasferir:"));
-            balancePesos -= trasferencia;
-            alert("¡Transferencia enviada!");
-        } else if (operacionA === "b") {
-            prompt("Ingrese numero de cuenta a depositar:");
-            deposito = parseFloat(prompt("Ingrese monto a depositar:"));
-            alert("!Depositado con éxito!");
-        } else if (operacionA === "v") {}
-    } else if (operacion === "b") {
-        let extraccion = prompt("A) Extraccion en pesos \nB)Extraccion en dolares \nV) Volver al menú principal");
-        extraccion = extraccion.toLowerCase();
-        if (extraccion === "a") {
-            extraccionPesos = parseFloat(prompt("¿Cuanto pesos queres extraer?"));
-            alert("¡Extraccion de $" + extraccionPesos + " exitosa!");
-        } else if (extraccion === "b") {
-            extraccionUsd = prompt("¿Cuantos USD queres extraer?");
-            alert("¡Extraccion exitosa!");
-        } else if (operacion === "v") {}
-    } else if (operacion === "c") {
-        let operacionC = prompt("A) Cuenta ahorro en pesos \nB) Cuenta dolares \nV) Volver al menú principal");
-        operacionC = operacionC.toLowerCase();
-        if (operacionC === "a") {
-            alert("Su saldo en pesos es de $" + (balancePesos - extraccionPesos));
-        } else if (operacionC === "b") {
-            alert("Su saldo en USD es de $" + (balanceUsd - extraccionUsd));
-        } else if (operacionC === "v") {}
+    gestionarExtraccion: function() {
+        let extraccion = prompt("A) Extracción en pesos \nB) Extracción en dólares \nV) Volver al menú principal").toLowerCase();
+        switch (extraccion) {
+            case "a":
+                this.extraccionPesos = parseFloat(prompt("¿Cuánto pesos querés extraer?"));
+                this.balancePesos -= this.extraccionPesos;
+                this.historialTransacciones.push(`Extracción de $${this.extraccionPesos} en pesos`);
+                alert("¡Extracción de $" + this.extraccionPesos + " exitosa!");
+                break;
+            case "b":
+                this.extraccionUsd = parseFloat(prompt("¿Cuántos USD querés extraer?"));
+                this.balanceUsd -= this.extraccionUsd;
+                this.historialTransacciones.push(`Extracción de $${this.extraccionUsd} en USD`);
+                alert("¡Extracción exitosa!");
+                break;
+            case "v":
+                break;
+            default:
+                alert("Opción no válida");
+        }
+    },
+
+    consultarSaldo: function() {
+        let operacionC = prompt("A) Cuenta ahorro en pesos \nB) Cuenta dólares \nV) Volver al menú principal").toLowerCase();
+        switch (operacionC) {
+            case "a":
+                alert("Su saldo en pesos es de $" + this.balancePesos);
+                break;
+            case "b":
+                alert("Su saldo en USD es de $" + this.balanceUsd);
+                break;
+            case "v":
+                break;
+            default:
+                alert("Opción no válida");
+        }
+    },
+
+    verHistorialTransacciones: function() {
+        if (this.historialTransacciones.length === 0) {
+            alert("No hay transacciones realizadas.");
+        } else {
+            alert("Historial de transacciones:\n" + this.historialTransacciones.join("\n"));
+        }
+    },
+
+    iniciar: function() {
+        this.iniciarSesion();
+
+        let continuar = true;
+        while (continuar) {
+            let operacion = prompt("Seleccione el tipo de operación que desea realizar \nA) Transferencia/Depósitos \nB) Extracciones/Adelantos \nC) Dinero en cuenta \nD) Ver historial de transacciones").toLowerCase();
+            this.gestionarOperacion(operacion);
+
+            continuar = confirm("¿Desea realizar otra operación?");
+        }
+
+        alert("Gracias por usar nuestro servicio");
     }
-}
+};
 
-let operacion = prompt("Seleccione el tipo de operación que desea operar \nA) Transferencia/Depósitos \nB) Extracciones/Adelantos \nC) Dinero en cuenta");
-gestionarOperacion(operacion);
 
-let continuar = true;
-
-while (continuar) {
-    let operacion = prompt("Seleccione el tipo de operación que desea operar \nA) Transferencia/Depósitos \nB) Extracciones/Adelantos \nC) Dinero en cuenta");
-    gestionarOperacion(operacion);
-
-    continuar = confirm("¿Desea realizar otra operación?");
-}
-
-alert("Gracias por usar nuestro servicio");
+Cajero.iniciar();
